8bb0da12a9e79a325cff1c3ab43ac46b2533b984
황현민 <102218665+hyunmin200@users.noreply.github.com>
2022-10-06 22:52:49 +0900

    FastCampusCs
    FastCampusCs

---
+++ FastCampusCs/FastCampus_Sample_CS/067_Check/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace _067_Check
+{
+    internal class Program
+    {
+        static void Start()
+        {
+            Console.WriteLine("성적 프로그램 - Method");
+        }
+
+        static void Input(ref int kor, ref int mat, ref int eng)
+        {
+
+            Console.Write("국어 성적 입력(정수)? ");
+            kor = int.Parse(Console.ReadLine());
+            Console.Write("수학 성적 입력(정수)? ");
+            mat = int.Parse(Console.ReadLine());
+            Console.Write("영어 성적 입력(정수)? ");
+            eng = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("kor: {0}, mat: {1}, eng: {2}", kor, mat, eng);
+        }
+
+        static int Total(int kor, int mat, int eng)
+        {
+            return kor + mat + eng;
+        }
+
+        static void average(int total, out float avg)
+        {
+            avg = (float)total / 3f;
+        }
+
+        static void Main(string[] args)
+        {
+            int kor = 0;
+            int eng = 0;
+            int mat = 0;
+            int total;
+            float avg;
+
+            Start();
+            Input(ref kor, ref mat, ref eng);
+            total = Total(kor, mat, eng);
+            average(total, out avg);
+
+            Console.WriteLine("Total: {0}   Average: {1}", total, avg);
+        }
+    }
+}
