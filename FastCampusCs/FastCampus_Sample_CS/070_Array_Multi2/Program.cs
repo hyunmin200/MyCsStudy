c6bb74181baba82814a3bbb484280c8d803c4d87
황현민 <102218665+hyunmin200@users.noreply.github.com>
2022-10-09 21:06:15 +0900

    FastCampusCs
    FastCampusCs

---
+++ FastCampusCs/FastCampus_Sample_CS/070_Array_Multi2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _070_Array_Multi2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arrNum = new int[3, 2];
+            int[,] arrNum2 = new int[,] { { 0, 1 }, { 2, 3 }, { 4, 5 } };
+            int[,] arrNum3 = { { 0,1},{ 2,3},{ 4,5} };
+            
+            foreach(int temp in arrNum2)
+            {
+                Console.Write(" " + temp);
+            }
+
+            Console.Write("\n arrMulti.Length: " + arrNum2.Length);
+
+            foreach (int temp in arrNum3)
+            {
+                Console.Write(" " + temp);
+            }
+
+            Console.Write("\n arrMulti.Length: " + arrNum3.Length);
+
+            int[,,] arrMulti = new int[4, 3, 2];
+            //다른 선언방식은 너무 길어서 생략
+
+            Console.WriteLine("arrMulti.Length: " + arrMulti.Length);
+
+            foreach (int temp in arrMulti)
+            {
+                Console.WriteLine("  " + temp);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ FastCampusCs/FastCampus_Sample_CS/074_Array_Clear/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _074_Array_Clear
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[3];
+
+            Array.Clear(array, 0, array.Length);
+            for(int i = 0; i < array.Length; i++)
+            {
+                array[i] = i;
+            }
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.Write("  {0}  ", array[i]);
+            }
+            Console.WriteLine("\n----------------------------");
+
+            int[,] arrNum = new int[3, 2];
+
+            Console.WriteLine("\n---------- Array.Clear -----------");
+            Array.Clear(arrNum, 0, arrNum.Length);
+
+            for(int i = 0; i < arrNum.GetLength(0); i++)
+            {//arrNum.GetLength(0) => 3
+                for (int j = 0; j < arrNum.GetLength(1); j++)
+                {//arrNum.GetLength(1) => 2
+                    arrNum[i, j] = (i * arrNum.GetLength(1)) + j;
+                }
+            }
+
+            for(int i = 0; i < arrNum.GetLength(0); i++)
+            {
+                for(int j = 0; j < arrNum.GetLength(1); j++)
+                {
+                    Console.Write("  " + arrNum[i, j]);
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\n----------------------------");
+            
+            //3차원 배열은 생략
+        }
+    }
+}
+ 
