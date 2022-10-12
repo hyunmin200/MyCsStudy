de15c48e5654dd2215593e33c474e4d416c601cb
황현민 <102218665+hyunmin200@users.noreply.github.com>
2022-10-12 15:53:46 +0900

    FastCampusCs
    FastCampusCs

---
+++ FastCampusCs/FastCampus_Sample_CS/078_Constructor/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _078_Constructor
+{
+    class AA
+    {
+        int a;
+        float f;
+
+        public AA()
+        {
+            a = 0;
+            f = 0f;
+            Console.WriteLine("   Call Constructor  ");
+            Console.WriteLine("   a: {0}", a);
+            Console.WriteLine("   f: {0}", f);
+        }
+        public AA(int _a)
+        {
+            a = _a;
+
+            Console.WriteLine("   Call Constructor  ");
+            Console.WriteLine("   a: {0}", a);
+            Console.WriteLine("   f: {0}", f);
+        }
+
+        public AA(int _a, float _f)
+        {
+            a = _a;
+            f = _f;
+
+            Console.WriteLine("   Call Constructor  ");
+            Console.WriteLine("   a: {0}", a);
+            Console.WriteLine("   f: {0}", f);
+        }
+
+        ~AA()
+        {
+            Console.WriteLine("   Call Destructor   ");
+        }
+        //소멸자에는 어떤 파라미터도 넣을 수 없음
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //생성자가 하나라도 생성이 되어있다면 기본 생성자는 생성되지 않음
+            AA aa = new AA();
+            AA aaa = new AA(10);
+            AA aaaa = new AA(100, 0.5f);
+            //오버로딩된 생성자
+        }
+    }
+}
---
+++ FastCampusCs/FastCampus_Sample_CS/080_static/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _080_static
+{
+    internal class Program
+    {
+        class AA
+        {
+            public static int a;
+            public static int b;
+            public static readonly int c = 200; // 상수화 된다.
+            // readonly 읽기 전용
+
+            public int num;
+
+            public static void Print()
+            {
+                Console.WriteLine("a: {0}", a);
+                Console.WriteLine("b: {0}", b);
+
+                //num = 100; //static속성의 메서드는 static속성인 변수만 사용 가능함
+            }
+        }
+
+        class BB
+        {
+            public int a;
+            public int b;
+
+            public void Print()
+            {
+                Console.WriteLine("a: {0}", a);
+                Console.WriteLine("b: {0}", b);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            AA.a = 10; //객체 생성 없이 바로 접근..
+            AA.b = 100; //객체 생성 없이 바로 접근..
+            //AA.c = 200; // 오류: readonly이므로 변경 불가..
+
+            AA.Print(); //객체 생성 없이 바로 접근..
+
+            //BB.a //오류
+            //BB.b //오류
+            //BB.Print(); //오류..
+
+            BB bb = new BB();
+            bb.a = 100;
+            bb.b = 200;
+
+            bb.Print();
+        }
+    }
+}
