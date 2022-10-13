4cf8445639bf825c4072075c73f6643aadfdd86a
황현민 <102218665+hyunmin200@users.noreply.github.com>
2022-10-13 22:23:27 +0900

    FastCampusCs
    FastCampusCs

---
+++ FastCampusCs/FastCampus_Sample_CS/091_Property01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _091_Property01
+{
+    class AA
+    {
+        private int num;
+
+        public int NUM
+        {
+            get { return num; }
+            set { num = value; } //set부분을 구현하지 않으면 readonly와 같은 읽기 전용
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AA aa = new AA();
+            //aa.num = 100; // 정보은닉
+            aa.NUM = 100; //set..
+            
+            Console.WriteLine("aa.NUM: {0}", aa.NUM); // get..
+        }
+    }
+}
